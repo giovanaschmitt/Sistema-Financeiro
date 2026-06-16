d97cce6c7f47288113bf95f182f7609ba6f9b51c
Giovana Schmitt <159589095+giovanaschmitt@users.noreply.github.com>
2026-06-15 21:59:27 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'financas-casal-v1';
+const ASSETS = [
+  '/index.html',
+  '/css/style.css',
+  '/js/app.js',
+  '/js/ocr.js',
+  '/js/charts.js',
+  '/js/storage.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS).catch(() => {}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached))
+  );
+});
